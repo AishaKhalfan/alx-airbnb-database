002516332296b27cfc6915431319aa1ad9788a8d
AishaKhalfan <aishakhalfan45@gmail.com>
2025-05-17 15:04:21 +0300

    oder by

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -12,6 +12,8 @@ FROM
   properties
 LEFT JOIN
   reviews ON properties.property_id = reviews.property_id;
+ORDER BY
+  properties.name;
 
 
 --Write a query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user. 
